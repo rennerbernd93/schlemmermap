7370ae18daaf10e0592f1b907a5385678e9070ae
Bernd Renner <renner.bernd93@gmail.com>
2025-11-17 23:53:10 +0100

    fix supabase default export, mail exports, request-password page

---
+++ pages/request-password.js
@@ -0,0 +1,8 @@
+export default function RequestPasswordPage() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Passwort anfordern</h1>
+      <p>Diese Seite ist aktuell leer, wird aber benötigt, damit Next.js builden kann.</p>
+    </div>
+  );
+}
